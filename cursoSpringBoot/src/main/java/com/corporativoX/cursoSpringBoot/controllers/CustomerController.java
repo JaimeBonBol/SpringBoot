319c58d1830cec67c173e2f1b388263f621bcc5c
JaimeBonBol <jaimebb2002@gmail.com>
2025-08-05 12:11:44 +0200

    Enrutamiento de URL en Spring Boot

--- cursoSpringBoot/src/main/java/com/corporativoX/cursoSpringBoot/controllers/CustomerController.java
+++ cursoSpringBoot/src/main/java/com/corporativoX/cursoSpringBoot/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package com.corporativoX.cursoSpringBoot.controllers;
 
 import com.corporativoX.cursoSpringBoot.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,6 +22,16 @@ public class CustomerController {
     @GetMapping("/clientes")
     public List<Customer> getCustomers(){
         return customers;
+    }
+
+    @GetMapping("/clientes/{username}")
+    public Customer getCustomerByUsername(@PathVariable String username){
+        for (Customer customer : customers){
+            if (customer.getUserName().equalsIgnoreCase(username)){
+                return customer;
+            }
+        }
+        return null;
     }
 
 }
