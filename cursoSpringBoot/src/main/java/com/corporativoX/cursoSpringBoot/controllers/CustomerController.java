fb7e3653b0525bb3b1a56db02174391199a2eb4a
JaimeBonBol <jaimebb2002@gmail.com>
2025-08-05 15:11:46 +0200

    Anotaciones PostMapping y RequestBody

--- cursoSpringBoot/src/main/java/com/corporativoX/cursoSpringBoot/controllers/CustomerController.java
+++ cursoSpringBoot/src/main/java/com/corporativoX/cursoSpringBoot/controllers/CustomerController.java
@@ -1,9 +1,7 @@
 package com.corporativoX.cursoSpringBoot.controllers;
 
 import com.corporativoX.cursoSpringBoot.model.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,6 +30,12 @@ public class CustomerController {
             }
         }
         return null;
+    }
+
+    @PostMapping("/clientes")
+    public Customer postCustomer(@RequestBody Customer newCustomer){
+        customers.add(newCustomer);
+        return newCustomer;
     }
 
 }
